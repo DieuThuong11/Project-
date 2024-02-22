ff9a347d31ad3579a3ddd2f95d5c0570ecd9666e
DieuThuong11 <160484311+DieuThuong11@users.noreply.github.com>
2024-02-22 22:56:34 +0700

    Add files via upload

---
+++ SourceDemo/src/PhepTinh.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class PhepTinh {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 0;
+
+
+
+
+        System.out.println("Tong : " + (a+b));
+
+        System.out.println("Hieu : " + (a-b));
+
+        System.out.println("Tich : " + (a*b));
+
+        System.out.println("Khong the chia cho 0 " );
+
+
+
+
+
+
+    }
+
+}
+
